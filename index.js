f77704da35d3b972eb029c3f5ab39221a0ac3656
Valeria <itsval@gmail.com>
2022-01-27 18:58:41 -0500

    first commit

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let pageHeader = document.querySelector ('#page-header');
+pageHeader.style.backgroundColor = 'black';
+pageHeader.style.color = 'white';
 
-// Select some elements...
+let dogNames = document.querySelectorAll ('.dog-name');
+for (let i = 0; i < dogNames.length; i++) {
+    dogNames[i].stlye.color = "red";
+}
+
+let dogNames =  document.querySelectoryAll ('.dog-image');
+for (let i = 0; i < dogImages.length; i++) {
+    dogImages[i].style.borderRadius = "15px"
+}
+
